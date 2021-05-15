85a4de5ce52525eea7e09d95606c8a35fa99048e
Danilo Dionisia <danilo.dionisia@yahoo.com.br>
2021-05-15 16:49:24 -0300

    The user authentication is done

---
+++ src/config/strings.js
@@ -0,0 +1,39 @@
+exports.PASSWORD_EMPTY = {
+    error: 'Fill the password field'
+};
+
+exports.PASSWORD_TO_SHORT = {
+    error: 'The password field needs at least 6 charcters'
+};
+
+exports.PASSWORD_SPECIAL_CHARS = {
+    error: 'The password field needs numbers and special characters'
+};
+
+exports.NAME_EMPTY = {
+    error: 'Fill the name field'
+};
+
+exports.NAME_TO_SHORT = {
+    error: 'The name field needs at least 2 charcters'
+};
+
+exports.NAME_FORMAT = {
+    error: 'Fill the name field correctly'
+};
+
+exports.EMAIL_EMPTY = {
+    error: 'Fill the email field'
+};
+
+exports.EMAIL_FORMAT = {
+    error: 'Fill the email field correctly'
+};
+
+exports.USER_NOT_FOUND = {
+    error: 'User not found'
+};
+
+exports.USER_INVALID_PASSWORD = {
+    error: 'Invalid password'
+};
