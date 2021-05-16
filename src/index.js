0454f7a88f337ea40b364058bc4accd94db8136d
Danilo Dionisia <danilo.dionisia@yahoo.com.br>
2021-05-16 18:42:04 -0300

    change cors

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
