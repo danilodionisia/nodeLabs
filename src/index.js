0c10e85f707757c69122beae3e966cab5b919514
Danilo Dionisia <danilo.dionisia@yahoo.com.br>
2021-05-16 17:50:09 -0300

    fix port app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
